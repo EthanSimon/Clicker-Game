ef16a7a0697b5539d5a2012a2177b507629fd7fe
EvlCheese <ethanmsimon10@gmail.com>
2015-09-17 13:15:54 -0700

    Update click.js

--- click.js
+++ click.js
@@ -2,29 +2,15 @@ var click = 0;
 var autoClicks = 0;
 var number = 1;
 var addClickAmount = 0;
-
-function startTime() {
-    var today=new Date();
-    var h=(today.getHours() - today.getHours());
-    var m=(today.getMinutes() - today.getMinutes());
-    var s=(today.getSeconds() - today.getMinutes());
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('loadedSeconds').innerHTML = h+":"+m+":"+s;
-    var t = setTimeout(function(){startTime()},500);
-}
-
-function checkTime(i) {
-    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
+var totalTime = 0;
+var number2 = 1;
 
 function buyAuto(){
     var autoCost = Math.floor(10 * Math.pow(1.1, autoClicks));       //works out the cost of auto click
     if (click >= autoCost) {                                         //checks that the palyer can afford the auto click
         autoClicks = autoClicks + 1;                                 //increase number of Auto Clicks
         click = click - autoCost;                                    //remove clicks spent
-        document.getElementById('autoClicks').innerHTML = autoClicks; //update number of auto clicks for user
+        document.getElementById('autoClicks').innerHTML = autoClicks;//update number of auto clicks for user
         document.getElementById('click').innerHTML = click;          //update number of clicks for users
     };
     var nextCost = Math.floor(10 * Math.pow(1.1, autoClicks));       //work out the cost of next auto Click
@@ -48,13 +34,17 @@ function buyClickAmount() {
     document.getElementById('nextClickCost').innerHTML = nextClickCost;
     
     number++;                                                        //adds +1 to number which will increase amount added when 'Click Here' button is clicked.     ~~Credit to Michael Reese~~
+}                                             
+function timeRecord(number2){
+    totalTime = totalTime + number2;
+    document.getElementById('totalTime').innerHTML = totalTime;
+     
 }
 
+window.setInterval(function(){                                       
+    counterClick(autoClicks);                                        //adds autoClick value to total clicks everyone 1 second (1000ms)                         
+}, 1000);
 
-
-
-
-window.setInterval(function(){                                       //adds refresh to page everyone 1 second (1000ms)
-    counterClick(autoClicks);
-    
+window.setInterval(function(){
+    timeRecord(1);
 }, 1000);
